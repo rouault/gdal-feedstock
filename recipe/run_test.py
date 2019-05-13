1c4d4668eec3f4ccfdd643715cc8d80278040c42
csedwar <cedwards@kasei.local>
2019-05-13 11:08:44 -0700

    update to run_test.py
    typo...

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -9,7 +9,7 @@ from osgeo import osr
 from osgeo.gdal_array import *
 
 drivers = ['netCDF', 'HDF4', 'HDF5', 'GTiff', 'PNG', 'JPEG', 'GPKG', 'KEA', 
-    'JP2OpenJPEG', 'WCS', 'PDF', 'FTIS']
+    'JP2OpenJPEG', 'WCS', 'PDF', 'FITS']
 for driver in drivers:
     print(driver)
     assert gdal.GetDriverByName(driver)
