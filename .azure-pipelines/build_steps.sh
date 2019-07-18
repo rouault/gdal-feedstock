4cd14e411ee5d8bb42e42aeeda81d7487c2b7c21
Filipe Fernandes <ocefpaf@gmail.com>
2019-07-18 14:52:27 -0300

    conda-build=3.17.8 seems to work

--- .azure-pipelines/build_steps.sh
+++ .azure-pipelines/build_steps.sh
@@ -20,6 +20,8 @@ conda-build:
 CONDARC
 
 conda install --yes --quiet conda-forge-ci-setup=2 conda-build -c conda-forge
+# workaround until the pinning issued is fixed in conda-build
+conda install conda-build=3.17.8 --yes
 
 # set up the condarc
 setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
