90e0c8f3ee8cc4c2812284e5834bba8908434556
Filipe Fernandes <ocefpaf@gmail.com>
2019-07-21 16:58:37 -0300

    old icu

--- .azure-pipelines/build_steps.sh
+++ .azure-pipelines/build_steps.sh
@@ -22,7 +22,7 @@ CONDARC
 conda install --yes --quiet conda-forge-ci-setup=2 conda-build -c conda-forge
 # workaround until the pinning issued is fixed in conda-build
 conda install conda-build=3.17.8 --yes
-conda clean --all --yes
+conda clean --all -y
 
 # set up the condarc
 setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
