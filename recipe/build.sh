78bdc3fc5fab0020203c51b5f2687bd7651677f4
Sam Gillingham <gillingham.sam@gmail.com>
2021-03-25 14:33:38 +1000

    remove patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,6 @@ find ${PREFIX}/lib -name '*.la' -delete
 unset PYTHON
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 # Filter out -std=.* from CXXFLAGS as it disrupts checks for C++ language levels.
 re='(.*[[:space:]])\-std\=[^[:space:]]*(.*)'
