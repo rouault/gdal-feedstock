083510a5d14b6f308a6cea1e766bd3c5d385d01b
Sam Gillingham <gillingham.sam@gmail.com>
2021-07-16 14:03:07 +1000

    added kmlbase.lib

--- recipe/set_bld_opts.bat
+++ recipe/set_bld_opts.bat
@@ -91,9 +91,9 @@ set BLD_OPTS=%WIN64% ^
     WEBP_LIBS="%LIBARY_LIB%\libwebp.lib" ^
     ZSTD_CFLAGS="-I%LIBRARY_INC%" ^
     ZSTD_LIBS="%LIBRARY_LIB%\libzstd.lib" ^
-    LIBKML_DIR=%LIBRARY_PREFIX% ^
-    LIBKML_INCLUDE="%LIBRARY_INC%" ^
-    LIBKML_LIBS="%LIBRARY_LIB%\kmlconvenience.lib %LIBRARY_LIB%\kmldom.lib %LIBRARY_LIB%\kmlengine.lib %LIBRARY_LIB%\kmlregionator.lib %LIBRARY_LIB%\kmlxsd.lib %LIBRARY_LIB%\minizip.lib %LIBRARY_LIB%\uriparser.lib"
+    LIBKML_DIR="%LIBRARY_PREFIX%" ^
+    LIBKML_INCLUDE="-I%LIBRARY_INC%" ^
+    LIBKML_LIBS="%LIBRARY_LIB%\kmlbase.lib %LIBRARY_LIB%\kmlconvenience.lib %LIBRARY_LIB%\kmldom.lib %LIBRARY_LIB%\kmlengine.lib %LIBRARY_LIB%\kmlregionator.lib %LIBRARY_LIB%\kmlxsd.lib %LIBRARY_LIB%\minizip.lib %LIBRARY_LIB%\uriparser.lib"
 
 
 endlocal && set BLD_OPTS=%BLD_OPTS%
