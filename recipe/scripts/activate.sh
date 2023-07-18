0aa52888ff34634a4652319f5c58466c0ef7143e
Damien Ayers <damien@omad.net>
2023-07-19 09:37:52 +1000

    POSIX Compliant de/activate scripts

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 # Store existing GDAL env vars and set to this conda env
 # so other GDAL installs don't pollute the environment
 
-if [[ -n "$GDAL_DATA" ]]; then
+if [ -n "${GDAL_DATA:-}" ]; then
     export _CONDA_SET_GDAL_DATA="$GDAL_DATA"
 fi
 
-if [[ -n "$GDAL_DRIVER_PATH" ]]; then
+if [ -n "${GDAL_DRIVER_PATH:-}" ]; then
     export _CONDA_SET_GDAL_DRIVER_PATH="$GDAL_DRIVER_PATH"
 fi
 
 # On Linux GDAL_DATA is in $CONDA_PREFIX/share/gdal, but
 # Windows keeps it in $CONDA_PREFIX/Library/share/gdal
-if [ -d "$CONDA_PREFIX/share/gdal" ]; then
+if [ -d "${CONDA_PREFIX}/share/gdal" ]; then
     export GDAL_DATA="$CONDA_PREFIX/share/gdal"
     export GDAL_DRIVER_PATH="$CONDA_PREFIX/lib/gdalplugins"
 elif [ -d "$CONDA_PREFIX/Library/share/gdal" ]; then
@@ -24,7 +24,7 @@ fi
 
 # Support plugins if the plugin directory exists
 # i.e if it has been manually created by the user
-if [[ ! -d "$GDAL_DRIVER_PATH" ]]; then
+if [ ! -d "$GDAL_DRIVER_PATH" ]; then
     unset GDAL_DRIVER_PATH
 fi
 
