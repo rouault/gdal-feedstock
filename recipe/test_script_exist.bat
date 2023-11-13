6cf10f72fd3f43e8263abbc655d9d0d13d94297d
Peter Limkilde Svendsen <34237736+plimkilde@users.noreply.github.com>
2023-11-13 06:15:05 +0100

    Add tests for .py utilities (#835)
    * Add tests for .py utilities
    
    * Bump build number
    
    * Move calls to meta.yaml
    
    This reverts commit f60d0ef34d675d5954cf8ae617505e94117ac699.
    
    * Check only for existence of gdalattachpct.py
    
    * Check only for existence of gdal_retile.py
    
    * Check only for existence of gdal_proximity.py
    
    * Check only for existence of gdal_edit.py
    
    * Fix comment
    
    * Check only for existence of gdal_pansharpen.py
    
    * Only test for existence of ogrmerge.py
    
    * Check only for existence for ogr_layer_algebra.py
    
    * Try disabling existence checks on Windows
    
    * Re-enable existence checks in separate batch file
    
    * Whoops, run .bat file only on Windows
    
    * Update recipe/meta.yaml
    
    Co-authored-by: Xylar Asay-Davis <xylarstorm@gmail.com>
    
    * Try existence check in %LIBRARY_BIN%
    
    * Use single backslashes in batch file paths
    
    * Try to print path to .py files in output
    
    * Check in %PREFIX%\Scripts, clean up comments
    
    ---------
    
    Co-authored-by: Xylar Asay-Davis <xylarstorm@gmail.com>

---
+++ recipe/test_script_exist.bat
@@ -0,0 +1,21 @@
+@echo off
+REM As a workaround for GDAL apps unexpectedly returning error codes, this
+REM script checks only for the existence of the .py files below.
+REM This file should be replaced by calls to the scripts in meta.yaml once the
+REM behavior is fixed in GDAL itself.
+
+@echo on
+
+if not exist %PREFIX%\Scripts\gdalattachpct.py exit 1
+
+if not exist %PREFIX%\Scripts\gdal_retile.py exit 1
+
+if not exist %PREFIX%\Scripts\gdal_proximity.py exit 1
+
+if not exist %PREFIX%\Scripts\gdal_edit.py exit 1
+
+if not exist %PREFIX%\Scripts\gdal_pansharpen.py exit 1
+
+if not exist %PREFIX%\Scripts\ogrmerge.py exit 1
+
+if not exist %PREFIX%\Scripts\ogr_layer_algebra.py exit 1
