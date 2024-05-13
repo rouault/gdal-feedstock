ed369907a3dc956705427a253634c7227c8c8e3e
Peter Limkilde Svendsen <pelis@sdfi.dk>
2024-05-13 12:30:31 +0200

    Test entry points, call all Python apps with --help

--- recipe/test_script_exist.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM As a workaround for GDAL apps unexpectedly returning error codes, this
-REM script checks only for the existence of the .py files below.
-REM This file should be replaced by calls to the scripts in meta.yaml once the
-REM behavior is fixed in GDAL itself.
-
-@echo on
-
-if not exist %PREFIX%\Scripts\gdalattachpct.py exit 1
