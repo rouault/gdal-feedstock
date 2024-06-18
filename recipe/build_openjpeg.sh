f8138a0a71eeb03fc844d25f82191713f7203b5d
Isuru Fernando <isuruf@gmail.com>
2024-06-18 17:10:10 -0500

    openjpeg is a raster driver

--- recipe/build_openjpeg.sh
+++ recipe/build_openjpeg.sh
@@ -10,8 +10,8 @@ cp CMakeCache.txt.orig CMakeCache.txt
 # turn on the required dependency and drivers
 cmake -DBUILD_PYTHON_BINDINGS:BOOL=OFF \
       -DGDAL_USE_OPENJPEG=ON \
-      -DOGR_ENABLE_DRIVER_OPENJPEG=ON \
-      -DOGR_ENABLE_DRIVER_OPENJPEG_PLUGIN=ON \
+      -DGDAL_ENABLE_DRIVER_OPENJPEG=ON \
+      -DGDAL_ENABLE_DRIVER_OPENJPEG_PLUGIN=ON \
       ${SRC_DIR}
 
 cmake --build . -j ${CPU_COUNT} --config Release
