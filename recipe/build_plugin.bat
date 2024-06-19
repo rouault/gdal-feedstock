1206538a62f3b5b06564cf40c2875dd691526ace
Isuru Fernando <isuruf@gmail.com>
2024-06-19 13:44:29 -0500

    Fix testing and simplify build script

---
+++ recipe/build_plugin.bat
@@ -0,0 +1,40 @@
+cd build
+if errorlevel 1 exit 1
+
+copy CMakeCache.txt.orig CMakeCache.txt
+
+REM Remove build artifacts potentially generated with a different libarrow version
+if "%PKG_NAME%" == "libgdal-arrow-parquet" (
+  rmdir ogr\ogrsf_frmts\arrow /s /q
+  rmdir ogr\ogrsf_frmts\parquet /s /q
+)
+
+if "%PKG_NAME%" == "libgdal-arrow-parquet" (
+  set "CMAKE_ARGS=%CMAKE_ARGS% ^
+      -DGDAL_USE_PARQUET=ON ^
+      -DGDAL_USE_ARROW=ON ^
+      -DGDAL_USE_ARROWDATASET=ON ^
+      -DOGR_ENABLE_DRIVER_ARROW=ON ^
+      -DOGR_ENABLE_DRIVER_ARROW_PLUGIN=ON ^
+      -DOGR_ENABLE_DRIVER_PARQUET=ON ^
+      -DOGR_ENABLE_DRIVER_PARQUET_PLUGIN=ON"
+)
+
+if "%PKG_NAME%" == "libgdal-openjpeg" (
+  set "CMAKE_ARGS=%CMAKE_ARGS% ^
+      -DGDAL_USE_OPENJPEG=ON ^
+      -DGDAL_ENABLE_DRIVER_JP2OPENJPEG=ON ^
+      -DGDAL_ENABLE_DRIVER_JP2OPENJPEG_PLUGIN=ON"
+)
+
+
+REM We reuse the same build directory as libgdal, so we just to have to
+REM turn on the required dependency and drivers
+cmake -DBUILD_PYTHON_BINDINGS:BOOL=OFF %CMAKE_ARGS% "%SRC_DIR%"
+if errorlevel 1 exit /b 1
+
+cmake --build . -j %CPU_COUNT% --verbose --config Release
+if errorlevel 1 exit /b 1
+
+cmake --build . --target install --config Release
+if errorlevel 1 exit /b 1
--- recipe/test_arrow_parquet.bat
+++
@@ -1,8 +0,0 @@
-echo %GDAL_DRIVER_PATH%
-dir %GDAL_DRIVER_PATH%
-
-ogrinfo --format Arrow
-if errorlevel 1 exit 1
-
-ogrinfo --format Parquet
-if errorlevel 1 exit 1
