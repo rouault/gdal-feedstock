05105c2e3ccadfb0f79d6df51fe671a65094442c
Isuru Fernando <isuruf@gmail.com>
2024-07-03 16:26:54 -0500

    fix build failures

--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -10,7 +10,7 @@ rm -rf swig/python
 
 Python_LOOKUP_VERSION=$($PYTHON -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor)+'.'+str(sys.version_info.micro))")
 
-cmake "-UPython*" "-U*LATER_PLUGIN" "-UOGR_ENABLE_DRIVER_*" "-UGDAL_ENABLE_DRIVER_*" \
+cmake "-UPython*" "-U*LATER_PLUGIN" \
       -DPython_LOOKUP_VERSION=${Python_LOOKUP_VERSION} \
       -DBUILD_PYTHON_BINDINGS:BOOL=ON \
       -DGDAL_USE_EXTERNAL_LIBS=OFF \
