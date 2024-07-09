1c56acec2186b2826fac60e7e9e1559afc9ad193
Isuru Fernando <isuruf@gmail.com>
2024-07-09 23:04:47 +0300

    split netcdf

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -35,6 +35,7 @@ cmake -G "Ninja" ^
       -DGDAL_USE_LIBAEC=OFF ^
       -DGDAL_USE_KEA=OFF ^
       -DGDAL_USE_TILEDB=OFF ^
+      -DGDAL_USE_NETCDF=OFF ^
       -DOGR_ENABLE_DRIVER_ARROW=OFF ^
       -DOGR_ENABLE_DRIVER_PARQUET=OFF ^
       -DGDAL_ENABLE_DRIVER_JP2OPENJPEG=OFF ^
@@ -46,6 +47,7 @@ cmake -G "Ninja" ^
       -DGDAL_ENABLE_DRIVER_GRIB=OFF ^
       -DGDAL_ENABLE_DRIVER_KEA=OFF ^
       -DGDAL_ENABLE_DRIVER_TILEDB=OFF ^
+      -DGDAL_ENABLE_DRIVER_NETCDF=OFF ^
       -DOGR_REGISTER_DRIVER_ARROW_FOR_LATER_PLUGIN=ON ^
       -DOGR_REGISTER_DRIVER_PARQUET_FOR_LATER_PLUGIN=ON ^
       -DGDAL_REGISTER_DRIVER_JP2OPENJPEG_FOR_LATER_PLUGIN=ON ^
@@ -57,6 +59,7 @@ cmake -G "Ninja" ^
       -DGDAL_REGISTER_DRIVER_GRIB_FOR_LATER_PLUGIN=ON ^
       -DGDAL_REGISTER_DRIVER_KEA_FOR_LATER_PLUGIN=ON ^
       -DGDAL_REGISTER_DRIVER_TILEDB_FOR_LATER_PLUGIN=ON ^
+      -DGDAL_REGISTER_DRIVER_NETCDF_FOR_LATER_PLUGIN=ON ^
       -DOGR_DRIVER_ARROW_PLUGIN_INSTALLATION_MESSAGE="You may install it with with 'conda install -c conda-forge libgdal-arrow-parquet'" ^
       -DOGR_DRIVER_PARQUET_PLUGIN_INSTALLATION_MESSAGE="You may install it with with 'conda install -c conda-forge libgdal-arrow-parquet'" ^
       -DGDAL_DRIVER_JP2OPENJPEG_PLUGIN_INSTALLATION_MESSAGE="You may install it with with 'conda install -c conda-forge libgdal-jp2openjpeg'" ^
@@ -67,6 +70,7 @@ cmake -G "Ninja" ^
       -DOGR_DRIVER_XLS_PLUGIN_INSTALLATION_MESSAGE="You may install it with with 'conda install -c conda-forge libgdal-xls'" ^
       -DGDAL_DRIVER_KEA_PLUGIN_INSTALLATION_MESSAGE="You may install it with with 'conda install -c conda-forge libgdal-kea'" ^
       -DGDAL_DRIVER_TILEDB_PLUGIN_INSTALLATION_MESSAGE="You may install it with with 'conda install -c conda-forge libgdal-tiledb'" ^
+      -DGDAL_DRIVER_NETCDF_PLUGIN_INSTALLATION_MESSAGE="You may install it with with 'conda install -c conda-forge libgdal-netcdf'" ^
       "%SRC_DIR%"
 
 if errorlevel 1 exit /b 1
